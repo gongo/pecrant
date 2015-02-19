f4a5f182d803ff245a2d884092c49242849944eb
Wataru MIYAGUNI <gonngo@gmail.com>
2015-02-19 20:05:09 +0900

    Introduce shUnit2

---
+++ pecrant_test.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+. ./pecrant
+
+unset -f _pecrant
+
+_pecrant() {
+    cat <<EOF
+1111111  default virtualbox poweroff /path/to/foo
+2222222  default virtualbox running  /path/to/foo bar baz
+EOF
+}
+
+test_pecrant_list() {
+    local expect=$(cat <<EOF
+1111111  default virtualbox poweroff /path/to/foo
+2222222  default virtualbox running  /path/to/foo bar baz
+EOF
+          )
+    local actual=$(_pecrant_list)
+
+    assertEquals "${expect}" "${actual}"
+}
+
+test_pecrant_selected_ids() {
+    local expect=$(cat <<EOF
+1111111
+2222222
+EOF
+          )
+    local actual=$(_pecrant_selected_ids)
+
+    assertEquals "${expect}" "${actual}"
+}
+
+test_pecrant_dir() {
+    local expect=$(cat <<EOF
+/path/to/foo
+/path/to/foo bar baz
+EOF
+          )
+    local actual=$(_pecrant_dir)
+
+    assertEquals "${expect}" "${actual}"
+}
+
+. ./shunit2-2.1.6/src/shunit2
